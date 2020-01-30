6d1fa2845c9456bec0fed3f6d838a111f57175ec
Nurbek Jabashev <njabashev@na.hhcpr.hilton.com>
2020-01-30 10:43:03 +0600

    ToDo

---
+++ ToDoTaskHM/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ToDoTaskHM.MvcApplication" Language="C#" %>
---
+++ ToDoTaskHM/Models/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoTaskHM.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Tag { get; set; }
+        public bool? IsDone { get; set; }
+    }
+}
